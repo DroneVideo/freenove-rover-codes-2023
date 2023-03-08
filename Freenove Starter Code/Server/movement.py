8ec01eaacfcd54a51ae6c7307370222ac66fb1a3
Harsh ajwani <harshajwani@pal-nat186-138-213.itap.purdue.edu>
2023-03-08 15:28:19 -0500

    Testing turn

--- Freenove Starter Code/Server/movement.py
+++ Freenove Starter Code/Server/movement.py
@@ -7,7 +7,7 @@ def leftturn():
 
     #Testing Turn
     PWM.setMotorModel(0, 0, -1000, -1000)
-    time.sleep(2.5)
+    time.sleep(2)
     PWM.setMotorModel(0, 0, 0, 0)
 
 if __name__ == '__main__':
