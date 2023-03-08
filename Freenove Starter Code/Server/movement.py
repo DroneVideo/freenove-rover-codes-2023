a3c3293d4cd0c0e9cec5abd69130ccff23a54dc8
Harsh ajwani <harshajwani@pal-nat186-138-213.itap.purdue.edu>
2023-03-08 16:04:25 -0500

    Testing Turn

--- Freenove Starter Code/Server/movement.py
+++ Freenove Starter Code/Server/movement.py
@@ -1,12 +1,22 @@
 import time
 from Motor import *
 
-def leftturn():
+def leftpointturn():
     PWM = Motor()
     PWM.setMotorModel(0,0,0,0)  # Stop motors initially
 
     #Testing Turn
     PWM.setMotorModel(0, 0, -1000, -1000)
+    time.sleep(2)
+    PWM.setMotorModel(0, 0, 0, 0)
+    time.sleep(1)
+
+def rightpointturn():
+    PWM = Motor()
+    PWM.setMotorModel(0,0,0,0)  # Stop motors initially
+
+    #Testing Turn
+    PWM.setMotorModel(-1000, -1000, 0, 0)
     time.sleep(2)
     PWM.setMotorModel(0, 0, 0, 0)
     time.sleep(1)
