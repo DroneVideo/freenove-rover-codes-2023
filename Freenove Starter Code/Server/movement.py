47a1ab38e9bcc0068b08e27a60cb32a163388d23
Harsh ajwani <harshajwani@pal-nat186-128-73.itap.purdue.edu>
2023-03-22 19:14:35 -0400

    Right Pivot Turn working. Adding Left

--- Freenove Starter Code/Server/movement.py
+++ Freenove Starter Code/Server/movement.py
@@ -34,7 +34,19 @@ def rightpivotturn():
     PWM.setMotorModel(0, 0, 0, 0)
     time.sleep(1)
 
+def leftpivotturn():
+    PWM = Motor()
+    PWM.setMotorModel(0, 0, 0, 0) # Stops motors initially
+
+    PWM.setMotorModel(1000, 1000, -1000, -1000)
+    time.sleep(1.5)
+
+    PWM.setMotorModel(0, 0, 0, 0)
+    time.sleep(1)
+
 if __name__ == '__main__':
     stoprover(1)
     rightpivotturn()
+    stoprover(2)
+    leftpivotturn()
     stoprover(5)
