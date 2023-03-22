cb45e2bca747a3c64dff034ca061f978d901373b
Harsh ajwani <harshajwani@pal-nat186-128-73.itap.purdue.edu>
2023-03-22 19:06:34 -0400

    Testing Pivot Turn

--- Freenove Starter Code/Server/movement.py
+++ Freenove Starter Code/Server/movement.py
@@ -31,7 +31,8 @@ def rightpivotturn():
     PWM.setMotorModel(-1000, -1000, 1000, 1000)
     time.sleep(1)
 
-    self.stoprover(1)
+    PWM.setMotorModel(0, 0, 0, 0)
+    time.sleep(1)
 
 if __name__ == '__main__':
     stoprover(1)
